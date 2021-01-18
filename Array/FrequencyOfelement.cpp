4e54e42aa3ffcae2af2cfd943df6e6c4e60bf2e8
Abhishek <abhishekshrivastav1920@gmail.com>
2021-01-18 21:32:03 +0530

    0

---
+++ Array/FrequencyOfelement.cpp
@@ -0,0 +1,34 @@
+const int INT_MAX = 2147483647;
+const int INT_MIN = -2147483647;
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+void frequency(int *arr,int n)
+{
+    int i{0},j{0};
+
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]!=arr[j])
+        {
+            cout<<arr[j] <<" "<< i-j<<endl;
+            j = i;
+        }
+
+    }
+    if(arr[j]==arr[n-1])
+        cout<<arr[j]<<" "<<n-j<<endl;
+}
+
+int main()
+{
+    ios_base:: sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;  cin>>n;
+    int *arr = new int[n];
+    for(int i=0;i<n;i++)cin>>arr[i];
+    frequency(arr,n);
+
+}
