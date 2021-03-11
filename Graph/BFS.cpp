f7a3ecd869c9e7496321f555b7644176bf482e84
Abhishek Shrivastav <55507908+2023PHOENIX@users.noreply.github.com>
2021-03-11 15:49:41 +0530

    Add files via upload

--- Graph/BFS.cpp
+++ Graph/BFS.cpp
@@ -3,67 +3,115 @@ const int INT_MIN = -2147483647;
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long int ll;
+void code_out();
+void fast_out();
 
-void PrintGraph(vector<int> adj[],int v)
+void bfsHelper(vector<int> graph[],int v,bool visited[])
 {
-    for(int i=1;i<=v;i++)
-    {
-        for(int x : adj[i])
-            cout<<x<<" ";
-        cout<<"\n";
-    }
+	visited[v] = true;
+
+	queue<int> qu;
+	qu.push(v);
+
+	while(!qu.empty()){
+		
+		int v = qu.front();
+		qu.pop();
+
+		cout<<v<<" ";
+
+		for(int u : graph[v])
+		{
+			if(!visited[u])
+			{
+				visited[u] = true;
+				qu.push(u);
+			}
+		}
+
+	}
+}
+void BFS(vector<int> graph[],int v)
+{
+	bool visited[v];
+	fill(visited, visited + sizeof(visited),false);
+
+	for(int i=0;i<v;i++)
+	{
+		if(!visited[i])
+			bfsHelper(graph,i,visited);
+	}
 }
 
 
-void BFS(vector<int> adj[],int v)
+void dfsHelper(vector<int> graph[],int v,bool visited[])
 {
-    bool visited[v];
-    fill(visited,visited + sizeof(visited),false);
+	visited[v] = true;
+	cout<<v<<" ";
 
-    queue<int> qu;
-    qu.push(1);
-    visited[1] = true;
+	for(auto &u : graph[v])
+	{
+		if(!visited[u])
+			dfsHelper(graph,u,visited);
+	}
+}
 
-    while(!qu.empty())
-    {
-        int u = qu.front();
 
-        qu.pop();
+void DFS(vector<int> graph[],int v)
+{
+	bool visited[v];
+	fill(visited,visited + sizeof(visited),false);
 
-        cout<<u<<" ";
+	dfsHelper(graph,0,visited);
+}
 
-        for(int v : adj[u])
-            {
-                if(!visited[v])
-                {
-                    visited[v]  = true;
-                    qu.push(v);
-                }
-            }
-    }
+void addEdge(vector<int> graph[],int v,int u)
+{
+	graph[u].push_back(v);
+	graph[v].push_back(u);
 }
 int main()
 {
-    ios_base:: sync_with_stdio(false);
-    cin.tie(NULL);
+   code_out();
+   fast_out();
 
-    int v;  cin>>v;
-
-    int e;  cin>>e;
-
-    vector<int> adj[v+1];
-
-    for(int i=0;i<e;i++)
-    {
-        int a,b;    cin>>a>>b;
-        adj[a].push_back(b);
-        adj[b].push_back(a);
-    }
-
-    PrintGraph(adj,v);
-    
-
-    BFS(adj,1);
+   int v,e;	cin>>v>>e;
 
 
+   vector<int> graph[v];
+
+
+   for(int i=0;i<e;i++)
+   {
+   	int a,b;	cin>>a>>b;
+   	addEdge(graph,a,b);
+   }
+  
+   cout<<"Depth first search : "<<endl;
+  DFS(graph,v);
+
+  cout<<endl;
+
+  BFS(graph,v);
+
+
+
+}
+
+
+void fast_out()
+{
+   ios_base:: sync_with_stdio(false);
+   cin.tie(NULL);
+}
+void code_out()
+{
+	
+	#ifndef ONLINE_JUDGE
+    // for getting input from input.txt
+    freopen("input.txt", "r", stdin);
+    // for writing output to output.txt
+    //this can be opted out if you want to print the output to the sublime console
+    freopen("output.txt", "w", stdout);
+	#endif
 }
